9ebf4b12767b877b3ded50f0bb4036efd5524043
anti <antimony5292@gmail.com>
2024-12-29 00:47:26 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import DevilsRoulette from './components/DevilsRoulette'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <DevilsRoulette />
+    </div>
+  )
+}
+
+export default App
